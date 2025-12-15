40130bf7905e38961f0f4890cc8b2b76df6cb4ba
HonkSupreme <rileylipinski@gmail.com>
2025-12-15 13:33:26 -0600

    restructure again

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,8 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
-/** @type {import('@sveltejs/kit').Config} */
-const dev = process.env.NODE_ENV === 'development';
-
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: dev ? '' : '/clickerproject'
-		}
-	}
-};
-
-export default config;
+/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+}
